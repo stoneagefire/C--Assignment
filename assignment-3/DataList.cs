c7ca3f35381b3b0d9c6f051d65e2ace5df280897
Stoneagefireball <patelsoham22@gmail.com>
2021-12-28 21:03:16 +0530

    New assignment-3 commit

---
+++ assignment-3/DataList.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace assignment_3
+{
+    public class DataList
+    {
+        IDictionary<int, string> dict;
+        
+
+        public DataList(IDictionary<int, string> dict)
+        {
+            this.dict = dict;
+        }
+
+        public void DictAddition(int key, string value)
+        {
+            this.dict.Add(key, value);
+        }
+
+        public void AddNewDetail()
+        {
+            System.Console.WriteLine("Enter PM Name: ");
+            var name = System.Console.ReadLine();
+
+            System.Console.WriteLine("Enter year: ");
+            var year = System.Console.ReadLine();
+
+            DictAddition(int.Parse(year), name);
+        }
+
+        internal void FindPMByYear(int year)
+        {
+            int status = 0;
+            foreach (KeyValuePair<int, string> kvp in dict)
+            {
+                if (kvp.Key == year)
+                {
+                    System.Console.WriteLine("PM of year " + kvp.Key + " is " + kvp.Value);
+                }
+                else
+                {
+                    status+=1;
+                }
+            }
+
+            if(status==dict.Count)
+            {
+                System.Console.WriteLine("No PM data found for input year...");
+            }
+        }
+
+        public void Display()
+        {
+            if(dict.Count!=0)
+            {
+                var list = dict.Keys.ToList();
+                list.Sort();
+                foreach (var i in list)
+                {
+                    System.Console.WriteLine("Name: {0}\tYear: {1}", i, dict[i]);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No data to display");
+            }
+        }
+    }
+}
--- assignment-3/Program.cs
+++ assignment-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace assignment_3
 {
@@ -6,7 +7,47 @@ namespace assignment_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IDictionary<int, string> dict = new Dictionary<int, string>();
+            DataList obj = new DataList(dict);
+
+            bool flag = true;
+
+            while(flag)
+            {
+                System.Console.WriteLine("\n===========================================");
+                System.Console.WriteLine("1. Add new PM details");
+                System.Console.WriteLine("2. Find PM by year");
+                System.Console.WriteLine("3. List PM by year");
+                System.Console.WriteLine("4. Exit");
+                System.Console.WriteLine("===========================================");
+                System.Console.WriteLine("Enter choice : ");
+                var choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        obj.AddNewDetail();
+                        break;
+
+                    case 2:
+                        System.Console.WriteLine("Enter year : ");
+                        var year = Console.ReadLine();
+                        obj.FindPMByYear(int.Parse(year));
+                        break;
+
+                    case 3:
+                        obj.Display();
+                        break;
+
+                    case 4:
+                        flag = false;
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Invalid choice!");
+                        continue;
+                }
+            }
         }
     }
 }
